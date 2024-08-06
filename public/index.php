fedfc14f993e8d6565c511b39c146b266406a6f0
mojtaba <mojtaba.abedi1381@gmail.com>
2024-08-06 11:53:13 +0330

    add the routes to the index

--- public/index.php
+++ public/index.php
@@ -1,2 +1,18 @@
 <?php
 require '../helper.php';
+require_once '../vendor/autoload.php';
+
+use App\App;
+use App\Router;
+
+
+$app = new App();
+$router = new Router($app);
+
+$router->get('/', 'Controllers\HomeController@index');
+$router->get('/hello', 'Controllers\HomeController@hello');
+
+$request = $app->getRequest();
+$response = $router->dispatch($request);
+
+echo $response;
